5240c9b6f2213a112ce6d6786c784ebcf26b2664
Cyrus Rahman <crahman@gmail.com>
2022-09-15 04:59:14 -0600

    Replace use of signbit(int) macro since Microsoft Visual Studio doesn't like it.

--- SunRise.cpp
+++ SunRise.cpp
@@ -163,11 +163,11 @@ SunRise::testSunRiseSet(int k, double offsetDays, double latitude, double longit
   //
   if ((VHz[0] < 0) && (VHz[2] > 0)) {
     if (!hasRise ||
-	(signbit(riseTime - queryTime) == signbit(eventTime - queryTime) &&
-	 fabs(riseTime - queryTime) > fabs(eventTime - queryTime)) ||
-	(signbit(riseTime - queryTime) != signbit(eventTime - queryTime) &&
+	((riseTime < queryTime) == (eventTime < queryTime) &&
+	 labs(riseTime - queryTime) > labs(eventTime - queryTime)) ||
+	((riseTime < queryTime) != (eventTime < queryTime) &&
 	 (hasSet && 
-	  signbit(riseTime - queryTime) == signbit(setTime - queryTime)))) {
+	  (riseTime < queryTime) == (setTime < queryTime)))) {
       riseTime = eventTime;
       riseAz = az;
       hasRise = true;
@@ -175,11 +175,11 @@ SunRise::testSunRiseSet(int k, double offsetDays, double latitude, double longit
   }
   if ((VHz[0] > 0) && (VHz[2] < 0)) {
     if (!hasSet ||
-	(signbit(setTime - queryTime) == signbit(eventTime - queryTime) &&
-	 fabs(setTime - queryTime) > fabs(eventTime - queryTime)) ||
-	(signbit(setTime - queryTime) != signbit(eventTime - queryTime) &&
+	((setTime < queryTime) == (eventTime < queryTime) &&
+	 labs(setTime - queryTime) > labs(eventTime - queryTime)) ||
+	((setTime < queryTime) != (eventTime < queryTime) &&
 	 (hasRise && 
-	  signbit(setTime - queryTime) == signbit(riseTime - queryTime)))) {
+	  (setTime < queryTime) == (riseTime < queryTime)))) {
       setTime = eventTime;
       setAz = az;
       hasSet = true;
